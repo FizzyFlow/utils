4d0f92ad05729723041b934129b1391c7ae90eb1
jeka911 <jeka911@gmail.com>
2018-05-22 17:56:16 +0300

    v++, unit tests, readme

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    node_version: {
+      /////// ensure you are using the node version required by your project's package.json
+      options: {
+        alwaysInstall: false,
+        errorLevel: 'fatal',
+        globals: [],
+        maxBuffer: 200*1024,
+        nvm: false,           //// try to `nvm use` first
+        override: ''          ///// use version from package.json
+      }
+    },
+    mochacli: {
+      options: {
+        require: ['assert'],
+        reporter: 'spec',
+        bail: true,
+        timeout: 60000,
+        files: ['tests/*.js']
+      },
+      local: {
+        timeout: 25000
+      }
+    }
+  });
+
+  /**
+   * Custom task to dynamically configure the `mochacli.options.files` Array.
+   * All filepaths that match the given globbing pattern(s), which is specified
+   # via the `grunt.file.expand` method, will be sorted chronologically via each
+   * file(s) latest modified date (i.e. mtime).
+   */
+  grunt.registerTask('runMochaTests', function configMochaTask() {
+    var sortedPaths = grunt.file.expand({ filter: 'isFile' }, 'tests/**/*.js')
+      .map(function(filePath) {
+        return {
+          fpath: filePath,
+          modtime: fs.statSync(filePath).mtime.getTime()
+        }
+      })
+      .sort(function (a, b) {
+        return a.modtime - b.modtime;
+      })
+      .map(function (info) {
+        return info.fpath;
+      })
+      .reverse();
+
+    grunt.config('mochacli.options.files', sortedPaths);
+    grunt.task.run(['mochacli:local']);
+  });
+
+  grunt.loadNpmTasks('grunt-node-version');
+  grunt.loadNpmTasks('grunt-mocha-cli');
+
+  grunt.registerTask('test', ['node_version', 'runMochaTests']);
+
+};
---
+++ README.md
@@ -0,0 +1,22 @@
+# FizzyFlow Utils
+
+Installation:
+----
+
+```bash
+npm install FizzyFlow/utils#master --save
+```
+
+Usage: 
+----
+
+```javascript
+const {EventEmitter, HashMap, Timers, Storage} = require('fizzyflow-utils');
+```
+
+Run unit tests:
+----
+
+```bash
+grunt test
+```
